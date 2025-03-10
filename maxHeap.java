557309649f21a0ff52d0b36ccd1420ff3c5752a0
John <jbhumph@gmail.com>
2025-03-10 11:27:07 -0700

    Finished MaxHeap.

--- maxHeap.java
+++ maxHeap.java
@@ -1,21 +1,86 @@
 public class MaxHeap {
-    // Member variables of this class
     private int[] Heap;
     private int size;
-    private int maxsize;
- 
-    // Initializing front as static with unity
-    private static final int FRONT = 1;
- 
-    // Constructor of this class
-    public GFG(int maxsize)
-    {
- 
-        // This keyword refers to current object itself
+    private int maxsize; 
+
+    public MaxHeap(int maxsize) {
         this.maxsize = maxsize;
         this.size = 0;
- 
-        Heap = new int[this.maxsize + 1];
-        Heap[0] = Integer.MIN_VALUE;
-    }   
+
+        Heap = new int[this.maxsize];
+    }
+
+    // return position of selected node's parent
+    private int parent(int pos) {
+        return (pos - 1) / 2;
+    }
+
+    // return position of selected node's left child
+    private int leftChild(int pos) {
+        return (2 * pos) + 1;
+    }
+
+    // return position of selected node's right child
+    private int rightChild(int pos) {
+        return (2 * pos) + 2;
+    }
+
+    // return true if selected node is leaf
+    private boolean isLeaf(int pos) {
+        if (pos > (size / 2) && pos <= size) {
+            return true;
+        }
+        return false;
+    }
+
+    // swap two nodes of the heap
+    private void swap(int fpos, int spos) {
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+    // heapify the node at pos
+    private void maxHeapify(int pos) {
+        if (!isLeaf(pos)) {
+            if (Heap[pos] < Heap[leftChild(pos)] || Heap[pos] < Heap[rightChild(pos)]) {
+                if (Heap[leftChild(pos)] > Heap[rightChild(pos)]) {
+                    swap(pos, leftChild(pos));
+                    maxHeapify(leftChild(pos));
+                } else {
+                    swap(pos, rightChild(pos));
+                    maxHeapify(rightChild(pos));
+                }
+            }
+        }
+    }
+
+    // insert a node into the heap
+    public void insert(int element) {
+        Heap[size] = element;
+        int current = size;
+
+        while (Heap[current] > Heap[parent(current)]) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+        size++;
+    }
+
+    // remove and return the maximum element from the heap
+    public int extractMax() {
+        int popped = Heap[0];
+        Heap[0] = Heap[size--];
+        maxHeapify(0);
+        return popped;
+    }
+
+    public void print() {
+        for (int i = 0; i <= size / 2; i++) {
+            System.out.print(" PARENT : " + Heap[i] + " LEFT CHILD : " + Heap[2 * i + 1] + " RIGHT CHILD : " + Heap[2 * i + 2]);
+            System.out.println();
+        }
+    }
+
 }
